69d8787c0e87f61ed088fde96c4eb349e1e59ad0
Adam Zieliński <adam@adamziel.com>
2025-03-12 20:41:05 +0100

    Initial commit

---
+++ client.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+    $pdo = new PDO("mysql:host=127.0.0.1;dbname=test", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+$result = $pdo->exec("DROP TABLE IF EXISTS wptests_users");
+$result = $pdo->exec("
+CREATE TABLE wptests_users (
+	ID bigint(20) unsigned NOT NULL auto_increment,
+	decimal_column DECIMAL(10,2) NOT NULL DEFAULT 0,
+	float_column FLOAT(10,2) NOT NULL DEFAULT 0,
+	enum_column ENUM('a', 'b', 'c') NOT NULL DEFAULT 'a',
+	date_column DATE NOT NULL DEFAULT CURRENT_DATE,
+	PRIMARY KEY  (ID),
+)
+	");
+$result = $pdo->exec("INSERT INTO wptests_users (decimal_column, float_column, enum_column, date_column) VALUES (123.45, 678.90, 'b', '2024-02-14')");
+
+$stmt = $pdo->prepare("SELECT * FROM wptests_users WHERE ID > :id");
+$stmt->execute(['id' => 0]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+var_dump($row);
